cb0f0cded23baffcf4613ac3958c6ffeb423f768
sophia9999 <hih0919@gmail.com>
2021-12-22 01:03:42 +0900

    backjun 문제번호 2588

---
+++ selfstudy/src/backjun/Study01.java
@@ -0,0 +1,30 @@
+package backjun;
+
+import java.util.Scanner;
+
+class Study01{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		String B = Integer.toString(b);
+
+		int x = Integer.parseInt(B.substring(2));
+
+		System.out.println(a*x);
+		
+		x = Integer.parseInt(B.substring(1, 2));
+		
+		System.out.println(a*x);
+		
+		x = Integer.parseInt(B.substring(0, 1));
+		
+		System.out.println(a*x);
+		
+		System.out.println(a*b);
+		sc.close();
+	}
+}
