eda020971fb28ea10391ef2eb72992b72eed1a0e
sophia9999 <hih0919@gmail.com>
2022-01-11 22:43:45 +0900

    백준 문제번호 10871_더나은방법

--- selfstudy/src/backjun/BOJ10781_1.java
+++ selfstudy/src/backjun/BOJ10781_1.java
@@ -1,7 +1,10 @@
 package backjun;
 
 import java.util.Scanner;
-
+/**
+ * 메모리 31112KB 시간 436ms 
+ * 이게 더 나은 방법 인 것 같다.
+ */
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
