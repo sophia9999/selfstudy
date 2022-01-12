807ea5f715bb3396b4646fdddf6bab0c2d96c3b7
Inhye Hong <93500863+sophia9999@users.noreply.github.com>
2022-01-13 00:18:44 +0900

    클릭하면 이미지 바뀜

--- mozilla/scripts/main.js
+++ mozilla/scripts/main.js
@@ -7,9 +7,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'image/firefox-icon.png') {
+    if(mySrc === 'images/firefox-icon.png') {
         myImage.setAttribute('src', 'images/budapest.png');
     } else {
-        myImage.setAttribute('src', 'image/firefox-icon.png');
+        myImage.setAttribute('src', 'images/firefox-icon.png');
     }
 }
