07463a4ffdc90a93934ccfc23fbce693a1031259
Inhye Hong <93500863+sophia9999@users.noreply.github.com>
2022-01-12 23:47:34 +0900

    예제 진행중

--- mozilla/scripts/main.js
+++ mozilla/scripts/main.js
@@ -1,2 +1,15 @@
+/*
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'image/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/budapest.png');
+    } else {
+        myImage.setAttribute('src', 'image/firefox-icon.png');
+    }
+}
