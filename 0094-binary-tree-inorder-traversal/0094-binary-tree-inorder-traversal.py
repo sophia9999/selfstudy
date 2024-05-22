93636a895564fceb7b6e9b0498736a47e00c5d7c
Inhye Hong <93500863+sophia9999@users.noreply.github.com>
2024-05-22 16:02:04 +0900

    Time: 35 ms (54.65%), Space: 16.5 MB (79.51%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -8,21 +8,19 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # 트리 순회: 중위 순회알고리즘
         # 중위 순회는 트리의 왼쪽을 먼저 방문하고 오른쪽 노드 방문 전에 부모노드를 방문한다.
-        
         result = []
         visitedNode = []
         
         while root or visitedNode:
             while root : # 트리의 왼쪽으로 가기 전에 방문한 parentNode를 저장해둔다.
                 visitedNode.append(root)
-                root = root.left #제일마지막 노드에 방문하면 root는 이제 끝
+                root = root.left #제일마지막 노드에 방문하면 root는 끝 / 밑에서 root.right로 돌아왔을 때 left가 있으면 여기를 탐.
             
             # 방문 했던 노드의 값을 꺼낸다. 
+            # 그리고 오른쪽에있는 node로 위치를 옮긴다.
             root = visitedNode.pop()
             result.append(root.val)
 
             root = root.right
 
         return result
-
-        
