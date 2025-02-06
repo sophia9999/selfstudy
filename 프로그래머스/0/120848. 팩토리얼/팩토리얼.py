e5c5956f83ddd2beeefe0ecd7dc006ec1a5cc5fe
Hong Inhye <hih0919@gmail.com>
2025-02-06 15:04:41 +0900

    팩토리얼 직접구현한ver

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,6 +1,18 @@
 import math
 def solution(n):
-    answer = 1
     for i in range(10, 0, -1) :
         if math.factorial(i) <= n :
             return i
+
+
+# def solution(n):
+#     for i in range(10, 0, -1) :
+#         if factorial(i) <= n :
+#             return i
+
+# def factorial(num) :
+#     answer = 1
+#     for i in range(num, 0, -1) :
+#         answer *= i
+    
+#     return answer
