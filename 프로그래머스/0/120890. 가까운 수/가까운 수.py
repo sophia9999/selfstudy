5258c8e62531a7ae2183b8fdb3182923e1c04969
Hong Inhye <hih0919@gmail.com>
2025-02-10 23:10:09 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,5 @@
 def solution(array, n):
-    absolute_distance = [ abs(i-n) for i in array]
-    min_index = absolute_distance.index(min(absolute_distance))
+    absolute_distance = [ (abs(i-n), i) for i in array]
+    absolute_distance.sort()
 
-
-    answer = n - absolute_distance[min_index]
-    if answer not in array :
-        answer = n + absolute_distance[min_index]
-
-    return answer
+    return absolute_distance[0][1]
