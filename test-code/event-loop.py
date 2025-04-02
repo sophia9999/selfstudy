818298643412c0c48dbd94e9e5b1df7ae7b20e7f
Hong Inhye <hih0919@gmail.com>
2025-04-02 17:29:32 +0900

    Update event-loop.py
    I think it's more compact and understandable

--- test-code/event-loop.py
+++ test-code/event-loop.py
@@ -2,27 +2,27 @@ import asyncio
 import time
 
 # sleep을 통해 서로 cpu를 양보하며 실행하는 경우
-async def yield_task(name, delay):
+async def yield_task(name):
     for i in range(3):
         print(f"{name}: step {i} at {time.strftime('%X')}")
-        await asyncio.sleep(delay)
+        await asyncio.sleep(0) # 여기지점에서 양보
     print(f"{name} 완료 at {time.strftime('%X')}")
 
 # 양보하지 않고 CPU를 점유하는 경우
-async def task(name, delay):
+async def task(name):
     for i in range(3):
         print(f"{name}: step {i} at {time.strftime('%X')}")
     print(f"{name} 완료 at {time.strftime('%X')}")
 
 async def main():
     await asyncio.gather(
-        yield_task("yield_task1", 1),
-        yield_task("yield_task2", 1)
+        yield_task("yield_task1"),
+        yield_task("yield_task2")
     )
 
     await asyncio.gather(
-        task("task1", 1),
-        task("task2", 1)
+        task("task1"),
+        task("task2")
     )
 
 asyncio.run(main())
