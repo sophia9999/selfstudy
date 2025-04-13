3fa75964171075417f152632eceb30c303519480
Hong Inhye <hih0919@gmail.com>
2025-04-13 12:15:08 +0000

    modify for removing unnecessary variable

--- test-code/cpu_heavy_yield.py
+++ test-code/cpu_heavy_yield.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 
-async def cpu_heavy(name, delay):
+async def cpu_heavy(name):
     print(f"{name}: start at {time.strftime('%X')}")
     for i in range(10**8):
         if i % 2_000_000 == 0 :
@@ -9,15 +9,15 @@ async def cpu_heavy(name, delay):
             await asyncio.sleep(0)
     print(f"{name}: end at {time.strftime('%X')}")
 
-async def io_task(name, delay):
+async def io_task(name):
     print(f"{name} started at {time.strftime('%X')}")
-    await asyncio.sleep(delay)
+    await asyncio.sleep(1)
     print(f"{name} finished at {time.strftime('%X')}")
 
 async def main():
     await asyncio.gather(
-        cpu_heavy("cpu_heavy_task", 1),
-        io_task("io_task", 1)
+        cpu_heavy("cpu_heavy_task"),
+        io_task("io_task")
     )
 
 asyncio.run(main())
